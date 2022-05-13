14a34006887566629c0a8860cb034a246cef0272
Flavio Bernaski <fbernaski@gmail.com>
2022-05-12 22:36:19 -0300

    Testando funcionalidade

--- lab07/Monitor/main.c
+++ lab07/Monitor/main.c
@@ -16,75 +16,77 @@ Datas:
 #include "resource_monitor.h"
 
 /* --- funcoes --- */
-void* barbeiro();
-void* cliente(void* thread_id);
+void *barbeiro();
+
+void *cliente(void *thread_id);
 
 /* --- main --- */
-int main(int argc, char* argv[]){
-   if(argc < 2) return printf("./$ <num_de_clientes>\n");
-   int numClientes = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+    if (argc < 2) return printf("./$ <num_de_clientes>\n");
+    int numClientes = atoi(argv[1]);
 
-   printf("numClientes: %d \n",numClientes);
-   // Identificadores para as Threads. (1 barbeiro + numClientes)
-   int id[(1+numClientes)];
-   
-   for (int i=0; i < (1+numClientes); i++) id[i] = i;
+    printf("numClientes: %d \n", numClientes);
+    // Identificadores para as Threads. (1 barbeiro + numClientes)
+    int id[(1 + numClientes)];
 
-   pthread_t barbeirod;
-   pthread_t clientes[numClientes];
+    for (int i = 0; i < (1 + numClientes); i++) id[i] = i;
 
-   initMonitor();
+    pthread_t barbeirod;
+    pthread_t clientes[numClientes];
 
-   //criar thread barbeiro
-   pthread_create(&barbeirod, NULL, barbeiro, NULL);
+    initMonitor();
 
-   //criar threads clientes
-   for(int i=0; i<numClientes; i++)
-       pthread_create(&clientes[i], NULL, cliente, (void*) &id[i+1]);
+    //criar thread barbeiro
+    pthread_create(&barbeirod, NULL, barbeiro, NULL);
 
-   // Wait for the threads to finish
-   pthread_join(barbeirod, NULL);
-   for(int i=0; i<numClientes; i++) pthread_cancel(clientes[i]);
+    //criar threads clientes
+    for (int i = 0; i < numClientes; i++){
+        pthread_create(&clientes[i], NULL, cliente, (void *) &id[i + 1]);
+        pthread_join(clientes[i], NULL);
+    }
 
-   destroyMonitor();
+    // Wait for the threads to finish
+    pthread_join(barbeirod, NULL);
 
-   printf("Programa finalizado.\n");
-   pthread_exit(0);
+    destroyMonitor();
+
+    printf("Programa finalizado.\n");
+    pthread_exit(0);
 }
 
 
 /* --- funcoes ---*/
-void* cliente(void* thread_id){
-   int id = *((int*)thread_id);
+void *cliente(void *thread_id) {
+    int id = *((int *) thread_id);
 
-   //seed do rand()
-   srand(id);
-   int tempo;
+    //seed do rand()
+    srand(id);
+    int tempo;
 
-   while(1){
-       // dormir por um tempo aleatorio
-       tempo = 2;;
-       sleep(tempo);
+    while (1) {
+        // dormir por um tempo aleatorio
+        tempo = 2;
+        sleep(tempo);
 
-       // cliente chegou na barbearia
-       int status = put(id);
-       if(status == 0) printf("CLIENTE %d: Chegou para cortar cabelo.\n", id);
-       fflush(stdout);
-   }
+        // cliente chegou na barbearia
+        int status = put(id);
+        if (status == 0) printf("CLIENTE %d: Chegou para cortar cabelo.\n", id);
+        fflush(stdout);
+    }
 }
 
-void* barbeiro(){
+void *barbeiro() {
     int descansando = 1;
-   
-   while(descansando == 1){    //parar o loop apos cortar todos os cabelos
 
-       //Barbeiro espera o sinal
-       descansando = take();
-       
-       if(descansando == 1 ){
-           printf("BARBEIRO: cortou cabelo do cliente.\n");
+    while (descansando == 1) {    //parar o loop apos cortar todos os cabelos
+
+        //Barbeiro espera o sinal
+        descansando = take();
+
+        if (descansando == 1) {
+            printf("BARBEIRO: cortou cabelo do cliente.\n");
         }
-       fflush(stdout);
-   }
+        fflush(stdout);
+    }
 }
 
--- lab07/Monitor/resource_monitor.c
+++ lab07/Monitor/resource_monitor.c
@@ -12,43 +12,43 @@ int bufferCliente[3];
 int clienteCount;
 
 // Initialize the mutex and condition variables
-void initMonitor(){
+void initMonitor() {
     pthread_mutex_init(&the_mutex, NULL);
     pthread_cond_init(&condBarbeiro, NULL);
     pthread_cond_init(&clienteWait, NULL);
 
     clienteCount = 0;
-    for(int i=0; i<3; i++) bufferCliente[i] = -1;
+    for (int i = 0; i < 3; i++) bufferCliente[i] = -1;
     bufferCadeiraBarbeiro = -1;
 }
 
 
 //adiciona cliente se ainda tiver espaco
-int put(int thread_id){
+int put(int thread_id) {
 
     //evitar duplicacao, nao executar a funcao se a thread que a chamou ja tiver no buffer
     int flag = 0;
-    for(int i=0; i<clienteCount; i++)
-        if(thread_id == bufferCliente[i])
+    for (int i = 0; i < clienteCount; i++)
+        if (thread_id == bufferCliente[i])
             flag = 1;
-    if(thread_id == bufferCadeiraBarbeiro){
+    if (thread_id == bufferCadeiraBarbeiro) {
         flag = 1;
     }
-    if(flag == 1) return 1;
+    if (flag == 1) return 1;
 
     //programa a partir dessa linha
     pthread_mutex_lock(&the_mutex);
-    
+
     // Se não houver clientes, chama o barbeiro e ocupa sua cadeira
-    if(bufferCadeiraBarbeiro == -1){
+    if (bufferCadeiraBarbeiro == -1) {
         bufferCadeiraBarbeiro = thread_id;
-        clienteCount = 1;
+        clienteCount += 1;
         pthread_cond_signal(&condBarbeiro);
-    // Se houver clientes, menos de 4, então o cliente fica em uma das cadeiras de espera    
-    }else if(clienteCount<4){
+        // Se houver clientes, menos de 4, então o cliente fica em uma das cadeiras de espera
+    } else if (clienteCount < 4) {
         bufferCliente[clienteCount] = thread_id;
         clienteCount += 1;
-    //se não houver cadeira de espera, vai embora e não entra no buffer
+        //se não houver cadeira de espera, vai embora e não entra no buffer
     } else {
         printf("Cliente %d não achou lugar e foi embora.", thread_id);
     }
@@ -59,42 +59,35 @@ int put(int thread_id){
 
 
 //funcao exclusiva do barbeiro
-int take(){
+int take() {
     pthread_mutex_lock(&the_mutex); /* protect buffer */
-    int tipo;
 
     //barbeiro esperando por qualquer chamado
-    while(clienteCount < 1)             /* If there is something  in the buffer then wait */
+    while (clienteCount < 1)             /* If there is something  in the buffer then wait */
         pthread_cond_wait(&condBarbeiro, &the_mutex);
 
-    //se tem pelo menos 1 cliente
-    if(clienteCount >= 1){
-        
-        // desloca para esquerda os clientes que estão na cadeira de espera
-        for(int i=0; i<clienteCount; i++){
-            bufferCliente[i] = bufferCliente[i+1];
-        }
-    
-        //reseta a ultima posição do bufferCliente, pois esta saindo da espera para a cadeira
-        bufferCadeiraBarbeiro = bufferCliente[clienteCount];
-
-        //Simula o tempo de corte
-        sleep(5);
-        //manda sinal para que possam se juntar no buffer de novo
-        pthread_cond_signal(&clienteWait);
-
-        tipo = 1;
+    // desloca para esquerda os clientes que estão na cadeira de espera
+    for (int i = 0; i < 3; i++) {
+        bufferCliente[i] = bufferCliente[i + 1];
     }
+    bufferCliente[2] = 0;
 
+    //reseta a ultima posição do bufferCliente, pois esta saindo da espera para a cadeira
+    bufferCadeiraBarbeiro = -1;
+
+    //Simula o tempo de corte
+    sleep(5);
+    //manda sinal para que possam se juntar no buffer de novo
+    pthread_cond_signal(&clienteWait);
     pthread_mutex_unlock(&the_mutex); /* release the buffer */
 
     //retorna 1 se deu certo o corte
-    return tipo;
+    return 1;
 }
 
 
 // Cleanup -- would happen automatically at end of program
-void destroyMonitor(){
+void destroyMonitor() {
     pthread_mutex_destroy(&the_mutex); /* Free up the_mutex */
     pthread_cond_destroy(&condBarbeiro);      /* Free up consumer condition variable */
     pthread_cond_destroy(&clienteWait);      /* Free up consumer condition variable */
